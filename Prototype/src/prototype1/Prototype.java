87fcc9d17986e1d242d66e9b2ba158baf349c0ca
bqa5094 <bqa5094@206-L16.up.ist.local>
2015-10-28 21:24:34 -0400

    I RALLAY LOOOV YAH

---
+++ Prototype/src/prototype1/Prototype.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototype1;
+
+import prototype1.PrototypeFrame;
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import javax.swing.*;
+import javax.swing.border.Border;
+public class Prototype {
+    
+    XMLDecoder de, xe;
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        
+        int x;
+        int y;
+        for (x = 0; x < 10; x++)
+            System.out.println("Is it working?");
+        
+        for (x = 0; x < 5; x++)
+        {
+            for (y = 0; y <= x; y++)
+            System.out.print("*");
+            System.out.println();
+        }
+        
+        System.out.println("Hi?");
+       PrototypeFrame frame = new PrototypeFrame();
+     
+    }
+    
+}
